5f6a00ec3ac21b5a30543f7f980392339fd875e1
Wei Liao <wliao008@gmail.com>
2014-12-16 22:48:17 -0500

    Experiment with breaking up lines by characters
    For ex, "1.1 Test" would be turn into:
    
    1.1 test$1.1 Test
    .1 test$1.1 Test
    1 test$1.1 Test
    test$1.1 Test
    est$1.1 Test
    st$1.1 Test
    t$1.1 Test
    
    But memory consumption for this is high compare to breaking the line
    by words.
    
    To break up the entries in list.txt:
    By words: ~21 mb
    By characters: ~191 mb

--- build_data_redis.rb
+++ build_data_redis.rb
@@ -1,7 +1,7 @@
 #require 'rubygems'
 require "redis"
 
-def stub(line, r)
+def by_word(line, r)
 	array = line.downcase.gsub!(/ - /, ' ').split(' ')
 	len = array.length - 1
 	(0..len).each {|n|
@@ -10,11 +10,25 @@ def stub(line, r)
 	}
 end
 
+def by_character(line,r)
+	array = line.downcase.gsub!(/ - /, ' ').split('')
+	len = array.length - 1
+	(0..len).each{|n|
+		char = array.slice(0,1).join
+		if char != ' '
+			val = array.join() + "$#{line.chop}"
+			r.zadd('icd9',0,val)
+		end
+		array.shift
+	}
+
+end
+
 def do_work()
 	r = Redis.new
 	f = File.open('list.txt', 'r')
 	f.each_line do |line|
-		stub(line, r)
+		by_word(line, r)
 	end
 	f.close
 end
